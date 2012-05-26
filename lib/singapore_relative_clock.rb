507a04bb34ed64fc89ef2436ffc638d7d134a938
Dondon Vizcayno <dondonvizcayno@gmail.com>
2012-05-26 15:21:59 +0800

    Update lib/singapore_relative_clock.rb

--- lib/singapore_relative_clock.rb
+++ lib/singapore_relative_clock.rb
@@ -3,6 +3,12 @@ require_relative 'gmt_offset_finder'
 class SingaporeRelativeClock
 
 	def timeDifferenceTo(city)
+		if city == "Sydney" begin
+			-2
+		elsif city == "Singapore" begin
+			0
+		end
+		
 	end
 
 end
