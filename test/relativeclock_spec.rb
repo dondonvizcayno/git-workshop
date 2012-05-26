49c33828c978f82a1e3d123f68b8461d17c38576
Dondon Vizcayno <dondonvizcayno@gmail.com>
2012-05-26 15:19:31 +0800

    Update test/relativeclock_spec.rb

--- test/relativeclock_spec.rb
+++ test/relativeclock_spec.rb
@@ -8,5 +8,9 @@ describe SingaporeRelativeClock do
 	it "it should return 2.5 for Bangalore" do
 		@finder.offsetFor("Bangalore").should === -2.5	
 	end
+	
+	it "it should return -2 for Sydney" do
+		@finder.timeDifferenceTo("Sydney").should === -2
+	end
 
 end
